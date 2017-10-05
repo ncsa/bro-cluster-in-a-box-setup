f92fdcc5a91042193e55161555650f889c86cbb7
Justin Azoff <jazoff@illinois.edu>
2017-10-05 18:47:18 -0400

    cpu isolation done

--- tuning.sh
+++ tuning.sh
@@ -1,5 +1,12 @@
 function tuning::setup {
-    if [ ! -e /etc/modprobe.d/ixgb.conf ]; then
-        cp ${ROOT}/dist/ixgb.conf /etc/modprobe.d/
+    if [ ! -e /etc/modprobe.d/ixgbe.conf ]; then
+        cp ${ROOT}/dist/ixgbe.conf /etc/modprobe.d/
+    fi
+
+    if ! grep -q nohz_full /etc/default/grub; then
+        echo "Appending nohz_full=1-19,21-39 isolcpus=1-15,17-31 to kernel commandline"
+        perl -pi.back -e 's/quiet"/quiet nohz_full=1-19,21-39 isolcpus=1-15,17-31"/' /etc/default/grub
+        echo "Rebuilding initrd"
+        /sbin/grub2-mkconfig -o /boot/grub2/grub.cfg
     fi
 }
